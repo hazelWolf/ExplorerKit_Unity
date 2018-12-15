d7ec6d35c49a049d96f6081bdb63b78cab4e273b
bhomitb <bhomitb@outlook.com>
2018-12-15 13:44:41 +0530

    Updated to Unity 2019.1.0a11 and added AR core

---
+++ 2019.1.0a11/Assets/Scripts/Data/XML2JSON.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.IO;
+using Newtonsoft.Json;
+
+public class XML2JSON {
+    public XmlDocument ParseXMLData(string data){
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXml(data);
+        return doc;
+    }
+
+    public string JsonToXml(XmlDocument doc){
+        string json = JsonConvert.SerializeXmlNode(doc);
+        return json;
+    }
+
+    
+}
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # ExplorerKit_Unity
 ### All my learning and findings in unity (unity journal) 
->####  Unity 2018.2.14f1
+>####  Unity 2019.1.0a11
 * Xml to JSON convert using Newtonsoft - 13th July 2018
 * Deserializing JSON to .NET object using Newtonsoft - 7th November 2018
+* ARCore Package v1.6.0 added - 13 December 2018
