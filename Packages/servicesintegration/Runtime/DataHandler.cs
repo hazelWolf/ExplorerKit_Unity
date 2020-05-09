0f6d8f04fae17fa66ba793fd43bee98f7593e501
hazelWolf <bhomitb@outlook.com>
2020-05-09 14:56:51 +0530

    added common package

---
+++ Packages/servicesintegration/Runtime/DataHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+/* Author: bhomitb, 12 April 2020
+ * Latest Revision : 
+ * Handling data from API's and posting data to API using UnityWebRequest
+ */
+namespace wolf.hazel.helper
+{
+    public class DataHandler
+    {
+        public IEnumerator FetchData(string url, Action<string> callback)
+        {
+            if (!string.IsNullOrEmpty(url))
+            {
+                using (UnityWebRequest www = UnityWebRequest.Get(url))
+                {
+                    yield return www.SendWebRequest();
+                    if (www.isNetworkError || www.isHttpError)
+                    {
+                        Debug.LogError("Error fetch data from : " + url + "got response : " + www.error);
+                        callback(null);
+                    }
+                    else callback(www.downloadHandler.text);
+                }
+            }
+            else
+            {
+                Debug.LogError("Please provide a valid url");
+            }
+        }
+
+        public IEnumerator PostData(string url, WWWForm data, Action<string> callback)
+        {
+            if (!string.IsNullOrEmpty(url))
+            {
+                using (UnityWebRequest www = UnityWebRequest.Post(url, data))
+                {
+                    yield return www.SendWebRequest();
+                    if (www.isNetworkError || www.isHttpError)
+                    {
+                        Debug.LogError("Error fetch data from : " + url + "got response : " + www.error);
+                        callback(null);
+                    }
+                    else callback(www.downloadHandler.text);
+                }
+            }
+            else
+            {
+                Debug.LogError("Please provide a valid url");
+            }
+        }
+    }
+}
---
+++ Packages/servicesintegration/Runtime/DownloadHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+/* Author: bhomitb, 12 April 2020
+ * Latest Revision : 8 May 2020
+ * Handling downloads from API's using UnityWebRequest & Download handler
+ */
+namespace wolf.hazel.helper
+{
+    public class DownloadHelper
+    {
+        public IEnumerator DownlodImage(string url, Action<Texture2D> callback)
+        {
+            if (!string.IsNullOrEmpty(url))
+            {
+                using (UnityWebRequest w = UnityWebRequestTexture.GetTexture(url))
+                {
+                    yield return w.SendWebRequest();
+                    if (w.isNetworkError || w.isHttpError)
+                    {
+                        Debug.LogError(w.error);
+                        callback(null);
+                    }
+                    else
+                    {
+                        // Get the texture from download
+                        Texture2D texture = DownloadHandlerTexture.GetContent(w);
+                        callback(texture);
+                    }
+                }
+            }
+            else
+            {
+                Debug.LogError("Please provide a valid url");
+            }
+        }
+    }
+}
